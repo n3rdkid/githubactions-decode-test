68fdb3e36e5be0878b739311e1138767c6a9a304
Saurav Adhikari <sauravads123@gmail.com>
2021-11-10 09:39:47 +0545

    feat :basic user creation

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
 const app =express();
+app.use(require("cors")());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const firebaseAdmin = require("firebase-admin");
+
+const serviceAccount = require('./serviceAccountKey.json');
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+});
 
 
 
@@ -12,14 +22,30 @@ app.get("/",(req,res)=>{
         }
     })
 })
-app.post("/",(req,res)=>{
+
+
+app.post("/",async (req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password || email==='' || password===''){
+        return res.status(400).json({
+            error:"SOME TING WENT WONG!"
+        })
+    }
+    try{
+    const user = await firebaseAdmin.auth().createUser({ email, password });
     res.json({
         data:{
             type:'success',
-            data: ' YO LO !'
+            data: {'id':user.id,'email':user.email}
         }
     })
+}catch(e){
+    return res.status(400).json({
+        error:e
+    })
+}
 })
+
 app.listen(8080,()=>{
     console.log("LISTENING ON PORT 8080");
 })
